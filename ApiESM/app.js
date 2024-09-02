88442efb23f8e0cc35f1848e40ebe30b6954e3c1
emanuel <134736553+EmanuelM153@users.noreply.github.com>
2024-09-02 13:32:51 -0500

    Axelar installation and NFT interface - Installed axelarjs-sdk - Added to the API nft operations

--- ApiESM/app.js
+++ ApiESM/app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const { Web3 } = require("web3");
-const contractInfo = require("./constants.json");
+const abi = require("./nftContractABI.json");
 
-const address = "0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC";
+const address = 
+  "0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC";
 const privateKey =
   "0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027";
 const rpc =
   "https://psychic-telegram-px57g75gg7536vvj-9650.app.github.dev/ext/bc/devChain/rpc";
-
+const contractAddress =
+  "0xA8FdD25760C3c6ADc77f07f49ffF97540B66c64A";
 
 app.use(express.json());
 app.use((req, res, next) => {
@@ -17,13 +19,6 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 })
-
-
-
-
-app.get("/", (req, res) => {
-  res.send("Hola Bogotá!");
-});
 
 app.get("/contract-info", async (req, res) => {
   const lucasNet = new Web3(rpc);
@@ -55,62 +50,41 @@ app.post("/transfer-native-token", async (req, res) => {
   });
 });
 
-app.post("/tranfer-diamond", async (req, res) => {
-  const data = req.body;
-  const lucasNet = new Web3(rpc);
-  const account = lucasNet.eth.accounts.wallet.add(privateKey);
-  let diamonds = new lucasNet.eth.Contract(
-    contractInfo.abi,
-    contractInfo.address
-  );
-  const transferDiamond = await diamonds.methods
-    .transfer(data.receiver, data.amount)
-    .send({
-      from: account[0].address,
-    });
+const nftList = {
+  gold: "ipfs://...",
+  silver: "ipfs://..."
+}
 
+app.post("/obtain-nft", async (req, res) => {
+  const data = req.body;
+  const devChain = new Web3(rpc);
+  const account = devChain.eth.accounts.wallet.add(privateKey);
+  const nftContract = new devChain.eth.Contract(abi, contractAddress);
+  const tx = await nftContract.methods
+    .safeTransferFrom(account[0].address, data.pubKey, data.numESM)
+    .send({from: account[0].address})
+    .then(function(receipt){
+      console.log(receipt);
+    });
   res.send({
     message: "Ok",
     statusCode: 200,
-    txHash: transferDiamond.transactionHash,
-  });
-});
+  })
+})
 
-app.post("/transfer", async (req, res) => {
-  const data = req.body;
-  const lucasNet = new Web3(rpc);
-  const account = lucasNet.eth.accounts.wallet.add(privateKey);
-  let diamonds = new lucasNet.eth.Contract(
-    contractInfo.abi,
-    contractInfo.address
-  );
-  const tx = {
-    from: account[0].address,
-    to: data.receiver,
-    value: data.emeralds,
-  };
-  //   const gasEmeralds = await lucasNet.eth.estimateGas(tx);
-  const txReceipt = await lucasNet.eth.sendTransaction(tx);
-  const transferDiamond = await diamonds.methods
-    .transfer(data.receiver, data.diamonds)
-    .send({
-      from: account[0].address,
-    });
-  //   const gasDiamond = await diamonds.methods
-  //     .transfer(data.receiver, data.diamonds)
-  //     .estimateGas();
+app.get("/nft-info", async (req, res) => {
+  const data = req.query;
+  const devChain = new Web3(rpc);
+  const account = devChain.eth.accounts.wallet.add(privateKey);
+  const nftContract = new devChain.eth.Contract(abi, contractAddress);
+  const tx = await nftContract.methods
+    .ownerOf(data.id)
+    .call({from: address})
   res.send({
     message: "Ok",
-    statusCode: 200,
-    txHashDiamond: transferDiamond.transactionHash,
-    txHashEmeralds: txReceipt.transactionHash,
-    // gasDiamond: Number(gasDiamond),
-    // gasEmeralds: Number(gasEmeralds),
-  });
-});
-
-//0.9465
-// 0.000...1
+    address: tx,
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
