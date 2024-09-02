76684d1075e99e24178dcf512ce306fa8d6adadc
emanuel <134736553+EmanuelM153@users.noreply.github.com>
2024-09-01 20:33:58 -0500

    Useless modifications

--- src/main/java/com/hackathon/controller/JugadasController.java
+++ src/main/java/com/hackathon/controller/JugadasController.java
@@ -5,15 +5,16 @@ import com.hackathon.dto.peticiones.PeticionMinar;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
-
 import com.hackathon.services.ServicioMinar;
 
 @RestController
-@AllArgsConstructor
 @RequestMapping("/api/jugadas")
 public class JugadasController {
   private final ServicioMinar servMinar;
+
+  public JugadasController(ServicioMinar servMinar) {
+    this.servMinar = servMinar;
+  }
 
   @PostMapping("/minar")
   public void mover(PeticionMinar movimiento) {
